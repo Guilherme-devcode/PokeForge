651d00ca3a447547aa70127c6a15a656a67c9626
Guilherme Rocha <guilherme.mesquita.rocha@gmail.com>
2024-04-15 00:52:06 -0300

    FEAT: Adding return button

--- FrontEnd/src/pages/PokemonDetail/PokemonDetail.tsx
+++ FrontEnd/src/pages/PokemonDetail/PokemonDetail.tsx
@@ -1,14 +1,23 @@
-import "./style.css";
-
+import { IoIosReturnLeft } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 import Pokemon from "../../shared/components/Pokemon/Pokemon";
 
 function PokemonDetail() {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate("/");
+  };
+
   return (
-    <div className="header-container">
+    <>
+      <div className="header-container p-3 point" onClick={handleGoBack}>
+        <IoIosReturnLeft style={{ cursor: "pointer" }} size={40} />
+      </div>
       <div id="container-fluid h-100">
         <Pokemon />
       </div>
-    </div>
+    </>
   );
 }
 
